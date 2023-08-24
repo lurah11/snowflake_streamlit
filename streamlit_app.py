340fde161c3ceaf014a438025987e6eaa8f81d47
Indrajid-Lurah <indrajid.lurah@gmail.com>
2023-08-24 17:05:41 +0700

    add filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,14 @@ streamlit.header("Build Your Own Fruit Smothie")
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 streamlit.dataframe(my_fruit_list)
+
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # create multiselect 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_filter = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+
+filtered = my_fruit_list.loc[fruit_filter]
+
 #display the table 
-streamlit.dataframe(my_fruit_list)
+
+streamlit.dataframe(filtered)
