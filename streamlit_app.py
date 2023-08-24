42bdd6621441a7c746f28e884a2618c109cf2fef
Indrajid-Lurah <indrajid.lurah@gmail.com>
2023-08-24 16:45:18 +0700

    change content to foods

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit 
 
-streamlit.title("Hello World")
+streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+treamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header("Build Your Own Fruit Smothie")
