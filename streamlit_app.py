ef7a5434b93d782151744b76fb75604d4cd449ad
Indrajid-Lurah <indrajid.lurah@gmail.com>
2023-08-24 17:02:16 +0700

    add prepopulated fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # create multiselect 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 #display the table 
 streamlit.dataframe(my_fruit_list)
